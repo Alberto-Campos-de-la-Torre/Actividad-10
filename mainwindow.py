e3d09987f6e159a5106aef4f9ef90336ca62716b
Alberto Campos de la Torre <alberto.campos3077@alumnos.udg.mx>
2020-11-03 19:28:04 -0600

    Se añadio la funcion de guardar y abrir archivos para almacenar las particulas en formato JSON

--- mainwindow.py
+++ mainwindow.py
@@ -1,4 +1,4 @@
-from PySide2.QtWidgets import QMainWindow
+from PySide2.QtWidgets import QMainWindow, QFileDialog,QMessageBox
 from PySide2.QtCore import Slot
 from ui_mainwindow import Ui_MainWindow
 from particulas import Particulas
@@ -24,11 +24,58 @@ class MainWindow(QMainWindow):
         self.ui.Rojo.valueChanged.connect(self.setValue)
         self.ui.Azul.valueChanged.connect(self.setValue)
 
-    
+        self.ui.actionAbrir.triggered.connect(self.action_abrir_archivo)
+        self.ui.actionGuardar.triggered.connect(self.action_guardar_archivo)
+        
+
+    @Slot()
+    def action_abrir_archivo(self):
+       # print('Abrir Archivo')
+        ubicacion = QFileDialog.getOpenFileName(
+           self,
+           'Abrir Archivo',
+           '.',
+           'JSON (*.json)'
+        )[0]
+        if self.particulas.abrir(ubicacion):
+            QMessageBox.information(
+                self,
+                "Exito",
+                "Se abrió el archivo " + ubicacion
+            )
+        else:
+            QMessageBox.critical(
+                self,
+                "Error",
+                "Error al abrir el archivo " + ubicacion
+            )
+
+    @Slot()
+    def action_guardar_archivo(self):
+        #print('Guardar Archivo')
+        ubicacion = QFileDialog.getSaveFileName(
+            self,
+            'Guardar Archivo',
+            '.',
+            'JSON (*.json)'
+        )[0]
+        print(ubicacion)
+        if self.particulas.guardar(ubicacion):
+            QMessageBox.information(
+                self,
+                "Exito",
+                "Se pudo crear el archivo " + ubicacion
+            )
+        else:
+            QMessageBox.critical(
+               self,
+               "Error",
+               "No se pudo crear el archivo " + ubicacion
+            )
 
     @Slot()
     def click_agregarfinal(self):
-        IDD = self.ui.ID.value()
+        Id = self.ui.ID.value()
         Ox = self.ui.OrigenX.value()   
         Oy = self.ui.OrigenY.value() 
         Dx = self.ui.DestinoX.value()
@@ -38,7 +85,7 @@ class MainWindow(QMainWindow):
         Blue = self.ui.Azul.value()
         Green = self.ui.Verde.value()   
 
-        particula = Particula(IDD,Ox,Oy,Dx,Dy,Vel,Red,Blue,Green)
+        particula = Particula(Id,Ox,Oy,Dx,Dy,Vel,Red,Blue,Green)
         self.particulas.agregar_final(particula)
         
         #print(particula)
@@ -46,7 +93,7 @@ class MainWindow(QMainWindow):
 
     @Slot()
     def click_agregarinicio(self):
-        IDD = self.ui.ID.value()
+        Id = self.ui.ID.value()
         Ox = self.ui.OrigenX.value()   
         Oy = self.ui.OrigenY.value() 
         Dx = self.ui.DestinoX.value()
@@ -56,7 +103,7 @@ class MainWindow(QMainWindow):
         Blue = self.ui.Azul.value()
         Green = self.ui.Verde.value()
 
-        particula = Particula(IDD,Ox,Oy,Dx,Dy,Vel,Red,Blue,Green)
+        particula = Particula(Id,Ox,Oy,Dx,Dy,Vel,Red,Blue,Green)
         self.particulas.agregar_inicio(particula)
 
         #print(IDD,Ox,Oy,Dx,Dy,Vel,Red,Blue,Green)
--- particula.py
+++ particula.py
@@ -1,31 +1,43 @@
 from algoritmos import distanciaeuclidiana
 class Particula:
-    def __init__(self,idd=0, origenx=0, origeny=0, destinox=0, destinoY=0, velocidad=0,rojo=0, verde=0, azul=0, distancia=0.0):
-        self.__idd =idd           
-        self.__origenx =origenx
-        self.__origeny =origeny
-        self.__destinox =destinox
-        self.__destinoy =destinoY      
+    def __init__(self,Id=0, origen_x=0, origen_y=0, destino_x=0, destino_y=0, velocidad=0,rojo=0, verde=0,azul=0, distancia=0.0):
+        self.__Id =Id          
+        self.__origen_x =origen_x
+        self.__origen_y =origen_y
+        self.__destino_x =destino_x
+        self.__destino_y =destino_y    
         self.__velocidad =velocidad
         self.__rojo =rojo
         self.__verde =verde
         self.__azul =azul
-        self.distancia = distanciaeuclidiana(origenx,destinox,origeny,destinoY)
+        self.distancia = distanciaeuclidiana(origen_x,destino_x,origen_y,destino_y)
 
     def __str__(self):
         return (
-            'ID:' + str(self.__idd) + '\n' +
-            'Origen X:' + str(self.__origenx) + '\n' +
-            'Origen Y:' + str(self.__origeny) + '\n' +
-            'Destino X:' + str(self.__destinox) + '\n' +
-            'Destino Y:' + str(self.__destinoy) + '\n' +
-            'Velocidad:' + str(self.__velocidad) + '\n' +
-            'Color:'     +
-            'Rojo:' + str(self.__rojo) + '\n' +
-            'Verde:' + str(self.__verde) + '\n' +
-            'Azul:' + str(self.__azul) + '\n' +
+            'Id:' + str(self.__Id) + '\n' +
+            'origen_x:' + str(self.__origen_x) + '\n' +
+            'origen_y:' + str(self.__origen_y) + '\n' +
+            'destino_x:' + str(self.__destino_x) + '\n' +
+            'destino_y:' + str(self.__destino_y) + '\n' +
+            'velocidad:' + str(self.__velocidad) + '\n' +
+            'rojo:' + str(self.__rojo) + '\n' +
+            'verde:' + str(self.__verde) + '\n' +
+            'azul:' + str(self.__azul) + '\n' +
             'Distancia = ' + str(self.distancia) + '\n'
-        )             
+        )  
+
+    def to_dict(self):
+        return{
+            "Id":  self.__Id ,
+	        "origen_x": self.__origen_x,
+	        "origen_y":  self.__origen_y,
+        	"destino_x": self.__destino_x,
+        	"destino_y": self.__destino_y,
+	        "velocidad": self.__velocidad,
+	        "rojo": self.__rojo,
+	        "verde": self.__verde,
+	        "azul": self.__azul
+        }           
 
 #l01 = Particula("220203","56","102","89","76","100","78","0","98")
 #print(l01)
